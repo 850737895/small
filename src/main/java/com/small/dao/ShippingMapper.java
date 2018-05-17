d5bfa432325480846c711fc635794737f462b3dd
850737895 <850737895@qq.com>
2018-05-17 19:55:37 +0800

    收货地址

--- src/main/java/com/small/dao/ShippingMapper.java
+++ src/main/java/com/small/dao/ShippingMapper.java
@@ -1,6 +1,9 @@
 package com.small.dao;
 
 import com.small.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,14 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    int insertReturnId(Shipping shipping);
+
+    int deleteByIdAndUserId(@Param("userId") Integer userId,@Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping shipping);
+
+    Shipping selectByIdAndUserId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectAllByUserId(Integer userId);
 }
